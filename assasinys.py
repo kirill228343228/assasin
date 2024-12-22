96d905d2d58ef33b456c0100cbcca3ff60820177
kirill228343228 <162022433+kirill228343228@users.noreply.github.com>
2024-12-22 12:15:24 +0200

    Update assasinys.py

--- assasinys.py
+++ assasinys.py
@@ -1,10 +1,10 @@
 from character import Character
 import random
+eboi = random.randint(1,20)
 class Assasin(Character):
 
     def __init__(self, name, health=100, damage=20, defense=0):
         Character.__init__(self, name, health=150, damage=40, defense=0)
-        eboi = random.randint(1,20)
 
 
     def attack(self, target):
